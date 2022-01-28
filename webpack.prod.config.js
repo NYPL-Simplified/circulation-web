1edaf613c734db8248c048fc402a90ffa6130e05
Samantha Andrews <samantharaeandrews@gmail.com>
2022-01-28 16:43:57 -0700

    add terser webpack plugin to production build

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,8 +1,18 @@
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
+const { merge } = require("webpack-merge");
+const TerserPlugin = require("terser-webpack-plugin");
+const common = require("./webpack.common.js");
 
 var config = merge(common, {
-  mode: "production"
+  mode: "production",
+  devtool: "source-map",
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        /* additional options here */
+      }),
+    ],
+  },
 });
 
 module.exports = config;
